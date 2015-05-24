b5fab71aceb9e4afa70bafd92909ad5409c67a84
Ilnur Gallyamov <gallyamov@it-projects.info>
2015-05-24 17:15:58 +0500

    added left menu

--- website_sale_top_menu/controllers.py
+++ website_sale_top_menu/controllers.py
@@ -1,7 +1,2 @@
 # -*- coding: utf-8 -*-
 from openerp import http
-
-#class website_sale(website_sale.website_sale):
-#    def shop(self, page=0, category=None, search='', **post):
-#        website_sale.website_sale.shop(self, page=0, category=None, search='', **post)
-    
