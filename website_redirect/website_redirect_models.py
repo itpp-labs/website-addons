e7831d44fd1a65e7b1196c625f442e9139373cca
Ivan Yelizariev <yelizariev@it-projects.info>
2015-08-04 15:50:25 +0500

    [IMP] check redirect rules only when error 404 is occured.

--- website_redirect/website_redirect_models.py
+++ website_redirect/website_redirect_models.py
@@ -2,6 +2,7 @@ from openerp import api, models, fields, SUPERUSER_ID
 from openerp.http import request
 import fnmatch
 import werkzeug.utils
+from werkzeug.exceptions import NotFound
 
 class website_redirect(models.Model):
     _name = 'website.redirect'
@@ -27,18 +28,23 @@ class website_redirect(models.Model):
 class ir_http(models.AbstractModel):
     _inherit = 'ir.http'
 
-    def _dispatch(self):
+    def _handle_exception(self, exception, code=500):
+        if code == 404 or isinstance(exception, NotFound):
+            redirect = self.try_redirect()
+            if redirect:
+                return redirect
+        return super(ir_http, self)._handle_exception(exception, code=code)
+
+    def try_redirect(self):
         host = request.httprequest.environ.get('HTTP_HOST', '').split(':')[0]
         www, _, h = host.partition('.')
         if www == 'www':
             host = h
         path = request.httprequest.path
-        for redirect in request.env['website.redirect'].sudo().search([]):
-            if redirect.domain and redirect.domain != host:
-                continue
-
+        print 'try_redirect', host, path
+        for redirect in request.env['website.redirect'].sudo().search(['|', ('domain', '=', False), ('domain', '=', host)]):
             for rule in redirect.rule_ids:
                 if fnmatch.fnmatch(path, rule.pattern):
                     code = 302
                     return werkzeug.utils.redirect(rule.target, code)
-        return super(ir_http, self)._dispatch()
+        return None
