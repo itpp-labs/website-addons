872e70224cb1fd543abab8d89b8af14a3a077d29
Salton <smassally@idtlabs.sl>
2015-09-11 19:24:38 +0000

    website_forum_faq_edit: Edit FAQ Guidelines
    THis moudle allows you to edit FAQ guidlines either
    to debrand or customize

---
+++ website_forum_faq_edit/__openerp__.py
@@ -0,0 +1,13 @@
+{
+    'name': "Website forum edit faq",
+    'version': '1.0.0',
+    'author': 'Salton Massally',
+    'category': 'Website',
+    'website': 'idtlabs.sl',
+    'depends': ['website_forum'],
+    'data': [
+        'views.xml',
+        ],
+    'auto_install': True,
+    'installable': True
+}
