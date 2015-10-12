7984376b591b0c66805462b8bb7400361d4a5cca
Ildar Nasyrov <nasyrov@it-projects.info>
2015-10-12 14:59:27 +0500

    [ADD] web_login_background module

---
+++ web_login_background/__openerp__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "web_login_background",
+    'author': "IT-Projects LLC, Ildar Nasyrov",
+    'website': "https://twitter.com/nasyrov_ildar",
+    'category': 'Base',
+    'version': '0.1',
+    'depends': ['base'],
+    'data': [
+        'templates.xml',
+        'views/attachment.xml',
+    ],
+}
