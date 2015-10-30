8251578743789f56d001511c5e6cc5afcf9afd27
ilmir <ilmir.karamov@gmail.com>
2015-10-30 17:16:15 +0500

    Adding description for web_login_background

--- web_login_background/__openerp__.py
+++ web_login_background/__openerp__.py
@@ -4,6 +4,7 @@
     'author': "IT-Projects LLC, Ildar Nasyrov",
     'website': "https://twitter.com/nasyrov_ildar",
     'category': 'Base',
+    'images': ['images/module_pic.png'],
     'version': '1.0.0',
     'depends': ['base'],
     'data': [
