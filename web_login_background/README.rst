380eb5f3886103bd02fbd3caa24c8635ed572e76
ilmir <ilmir.karamov@gmail.com>
2015-11-10 19:55:16 +0500

    Updating for the web_login_background module

---
+++ web_login_background/README.rst
@@ -0,0 +1,6 @@
+Web Login Background
+====================
+
+Tested on Odoo 8.0 a9f27add088d9ce0b9853796cb433950e46d27ec
+
+
---
+++ web_login_background/__openerp__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Web login background",
+    'author': "IT-Projects LLC, Ildar Nasyrov",
+    'website': "https://twitter.com/nasyrov_ildar",
+    'category': 'Base',
+    'images': ['images/module_pic.png'],
+    'version': '1.0.0',
+    'depends': ['base'],
+    'data': [
+        'templates.xml',
+        'views/attachment.xml',
+    ],
+}
