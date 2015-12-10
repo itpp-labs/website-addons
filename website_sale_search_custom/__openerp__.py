e484250e28e49b12e311d1d3fbee235ff19d4518
Ivan Yelizariev <yelizariev@it-projects.info>
2015-12-10 09:12:08 +0500

    [DOC] update price

--- website_sale_search_custom/__openerp__.py
+++ website_sale_search_custom/__openerp__.py
@@ -6,7 +6,7 @@
     'license': 'GPL-3',
     'category': 'Custom',
     'website': 'https://twitter.com/yelizariev',
-    'price': 1.00,
+    'price': 9.00,
     'currency': 'EUR',
     'images': ['images/search.png'],
     'depends': ['website_sale', 'product_tags'],
