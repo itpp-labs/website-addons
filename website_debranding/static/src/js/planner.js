73226474b225ed5ceac4d6fb18706135725babf8
Ivan Yelizariev <yelizariev@it-projects.info>
2016-01-04 19:00:24 +0500

    [FIX] remove planner from website systray

---
+++ website_debranding/static/src/js/planner.js
@@ -0,0 +1,8 @@
+odoo.define('website_debranding.planner', function (require) {
+"use strict";
+
+require('web.planner.website');
+$('#oe_systray .o_planner_systray').hide()
+$('#oe_systray .o_planner_help').hide()
+
+});
