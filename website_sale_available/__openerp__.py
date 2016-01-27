0157b2822f0ce82ce4b5eed6f449d4f7bfaf51ef
ilmir <ilmir.karamov@gmail.com>
2016-01-27 12:04:04 +0500

    Description for website_sale_available (8.0)

--- website_sale_available/__openerp__.py
+++ website_sale_available/__openerp__.py
@@ -1,10 +1,14 @@
 {
     'name': "Sale only available products on Website",
+    'summary': """Sale only available products on Website""",
     'version': '1.0.0',
     'author': 'IT-Projects LLC, Ivan Yelizariev',
     'license': 'GPL-3',
     'category': 'Custom',
     'website': 'https://yelizariev.github.io',
+    'images': ['images/available.png'],
+    'price': 9.00,
+    'currency': 'EUR',
     'depends': ['website_sale'],
     'data': [
         'website_sale_available_views.xml',
