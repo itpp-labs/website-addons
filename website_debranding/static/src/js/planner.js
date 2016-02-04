47a6e6667198acc5c666da7231fd65744dec85d7
ufaks <excellm@yandex.ru>
2016-02-04 23:09:11 +0500

    fix

--- website_debranding/static/src/js/planner.js
+++
@@ -1,8 +0,0 @@
-odoo.define('website_debranding.planner', function (require) {
-"use strict";
-
-    require('web.planner.website');
-    $('.hidden-xs .o_planner_systray').hide();
-    $('.hidden-xs .o_planner_help').hide();
-
-});
