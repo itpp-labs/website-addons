31cdcff1da796279496fc0a8fb895499163c1129
ufaks <excellm@yandex.ru>
2016-02-04 15:35:56 +0500

    fix planner: hide it only in website menu

--- website_debranding/static/src/js/planner.js
+++ website_debranding/static/src/js/planner.js
@@ -1,8 +1,8 @@
 odoo.define('website_debranding.planner', function (require) {
 "use strict";
 
-require('web.planner.website');
-$('#oe_systray .o_planner_systray').hide()
-$('#oe_systray .o_planner_help').hide()
+    require('web.planner.website');
+    $('.hidden-xs .o_planner_systray').hide();
+    $('.hidden-xs .o_planner_help').hide();
 
 });
