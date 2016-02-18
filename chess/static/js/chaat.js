08f40225216772e4c3bbbe81f96536ce67814462
Dinar <gabbasov@it-projects.info>
2016-02-18 14:25:36 +0500

    ajax
    added ajax method

--- chess/static/js/chaat.js
+++ chess/static/js/chaat.js
@@ -3,21 +3,21 @@
 	
 	var ajax = require('web.ajax');
 	var bus = require('bus.bus');
-	
 	var data = {};
 	1
 	var chat = {
 		init_chat: function(){
-			/*инициализация данных*/
-
-
-			data.message = $("#message_text").val();
+			
+			data.message = $("#message_text").val(); /*get message*/
+			/*вытащить ник пользователя и время отправки сообщения*/
 			send_message(data);
 		},
 
 		get_message: function(data){
+			
 			// Gets the list of messages from the server ??????
-			// add messages
+			
+			/*display messages*/
 			$("#window_chat").append("<p><span class='user'>" + (data['user']) +
 			"</span>: " + (data['message']) + "<span class='time_message'>: " + 
 			(data['time']) + "</span></p>");
@@ -30,27 +30,41 @@
 		
 		poll: function() {
 			
-			/*тут что-то надо делать с модулем bus я так думаю*/
-			/*вызываем функцию показать сообщение*/
+			var self = this;
+
+            this.bus = bus.bus;
+            this.bus.on("notification", this, this.on_notification);
+            this.bus.start_polling();
+			/*получаем сообщение из сервера в случае
+			если сообщение было получено успешно вызываем функцию get_message
+			и передаем полученное сообщение*/
 			get_message(data);
+			/*если сообщение не было получено, т.е. запрос не получил ответа*
+			делаем повторный запрос через некоторое время*/
 		}
+		
+		on_notification: function(notification){
+			
+            var self = this;
+            var channel = notification[0];
+            var message = notification[1];
+		},
+		
 		
 		send_message: function(data){
 			
-			ajax.jsonRpc('/chat/message/', 'call', data).then(function ({
-				$('#chat-form #message-text').val('');
-			})
-			/*вытащить ник пользователя и время отправки сообщения*/
-			/*отправить пакет в функцию get_messages*/
-			
-			
+			ajax.jsonRpc('/chess/chat/message/', 'call', data).then(function(result){
+				$('#chat-form #message-text').val("");
+			}).fail(function(){
+				$("#window_chat").append("<p><span class='error'>Not connection with server</span></p>");
+			});
 		},
 		
 		msg_btn: function() {
 			$("#message_btn").click(send_message);
 			$("#chat_form").submit(function(event) {
 				init_chat();
-				event.preventDefault(); // отменяем обработку 
+				event.preventDefault();
 			});
 			
 			poll();
