7bffd44c4c7a2c178ecf90373d0af6db4796e5dd
Dinar <gabbasov@it-projects.info>
2016-02-18 11:45:23 +0500

    chat
    added ajax

--- chess/static/js/chaat.js
+++ chess/static/js/chaat.js
@@ -1,51 +1,59 @@
-
-	function init_chat(){
-		/*инициализация данных*/
-		send_message();
-	}
-
+(function() {
+	"use strict";
 	
-	// send message
-	function send_message() {
-		
-		/*нужно вытащить значение сообщения с текстового поля #message_text*/
-		/*вытащить ник пользователя и время отправки сообщения*/
-		/*сформировать пакет с сообщением формата JSON*/
-		/*отправить пакет в функцию get_messages*/
-		/*после завершения отправки сообщения очистить
-		текстовое поле от сообщения*/
-		$('#chat-form #message-text').val('');
+	var ajax = require('web.ajax');
+	var bus = require('bus.bus');
+	
+	var data = {};
+	1
+	var chat = {
+		init_chat: function(){
+			/*инициализация данных*/
 
-	}
-	
-	
-	/* Gets the list of messages from the server and 
-	appends the messages to the chatbox*/
-		
-	function get_message(data) {
-		// Gets the list of messages from the server ??????
-		// add messages
-		$("#window_chat").append("<p><span class='user'>" + (data['user']) +
-		"</span>: " + (data['message']) + "</p>");
-		
-		// scroll to bottom
-		$("#window_chat").each(function () {
-			this.scrollTop = this.scrollHeight;
-		});
-		
-		// wait for next
-		setTimeout("get_messages()", 2000);
 
-	}
-	
-	function msg_btn() {
-		$("#message_btn").click(send_message);
-		$("#chat_form").submit(function(event) {
-			setTimeout(function() {
+			data.message = $("#message_text").val();
+			send_message(data);
+		},
+
+		get_message: function(data){
+			// Gets the list of messages from the server ??????
+			// add messages
+			$("#window_chat").append("<p><span class='user'>" + (data['user']) +
+			"</span>: " + (data['message']) + "<span class='time_message'>: " + 
+			(data['time']) + "</span></p>");
+			
+			// scroll to bottom
+			$("#window_chat").each(function () {
+				this.scrollTop = this.scrollHeight;
+			});
+		},
+		
+		poll: function() {
+			
+			/*тут что-то надо делать с модулем bus я так думаю*/
+			/*вызываем функцию показать сообщение*/
+			get_message(data);
+		}
+		
+		send_message: function(data){
+			
+			ajax.jsonRpc('/chat/message/', 'call', data).then(function ({
+				$('#chat-form #message-text').val('');
+			})
+			/*вытащить ник пользователя и время отправки сообщения*/
+			/*отправить пакет в функцию get_messages*/
+			
+			
+		},
+		
+		msg_btn: function() {
+			$("#message_btn").click(send_message);
+			$("#chat_form").submit(function(event) {
 				init_chat();
-				event.preventDefault(); // отменяем обработку соб
-			}, 
-			100);
-		});
-		
-	}
+				event.preventDefault(); // отменяем обработку 
+			});
+			
+			poll();
+		};
+	};
+});
