a49c0568d0a758defe7e5843ad9ba4c57ba9326e
Ivan Yelizariev <yelizariev@it-projects.info>
2016-02-23 18:19:43 +0500

    [ADD] website_sale_quantity_hide

---
+++ website_sale_quantity_hide/__openerp__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Hide quantity field in web shop",
+    'summary': "Allows to sale only 1 item at once",
+    'author': 'IT-Projects LLC, Ivan Yelizariev',
+    'license': 'GPL-3',
+    'category': 'Website',
+    'website': 'https://twitter.com/yelizariev',
+    'version': '1.0.0',
+    'depends': ['website_sale'],
+    'data': [
+        'views/product.xml',
+        'views/templates.xml'
+    ],
+}
+
+
---
+++ website_sale_quantity_hide/controllers/website_sale_quantity_hide.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+
+from openerp import http
+from openerp.addons.website_sale.controllers.main import website_sale as website_sale_controller
+
