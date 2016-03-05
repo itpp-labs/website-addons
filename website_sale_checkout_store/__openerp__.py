fa31f800280d44b164f7f6fc6ef86d921c830288
Ilyas <rakhimkulov@it-projects.info>
2016-03-05 16:25:22 +0500

    dev 1
    Conflicts:
    	website_sale_checkout_store/__openerp__.py
    	website_sale_checkout_store/templates.xml
    	website_sale_checkout_store/tz

---
+++ website_sale_checkout_store/__openerp__.py
@@ -0,0 +1,18 @@
+{
+    'name': "Website sale checkout store",
+    'version': '1.0.0',
+    'summary': """Website sale checkout store""",
+    'author': 'IT-Projects LLC',
+    'license': 'LGPL-3',
+    'category': 'Website',
+    # 'images': ['images/pos-multi-session.png'],
+    'website': 'https://yelizariev.github.io',
+    # 'price': 30.00,
+    # 'currency': 'EUR',
+    'depends': ['website_sale'],
+    'data': [
+        'templates.xml'
+    ],
+    'installable': True,
+    'auto_install': False,
+}
