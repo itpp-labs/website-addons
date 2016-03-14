397791b845802b3d9e26cb37622dc585779a25f1
Ilyas <rakhimkulov@it-projects.info>
2016-03-14 13:09:41 +0500

    + use <t t-set="website_sale_order" t-value="website.sale_get_order()"/>  instead of controllers

--- website_sale_checkout_store/controllers/main.py
+++ website_sale_checkout_store/controllers/main.py
@@ -40,17 +40,3 @@ class website_sale(website_sale):
             return request.redirect('/shop/confirmation')
         else:
             return super(website_sale, self).payment()
-
-    @http.route(['/shop/confirm_order'], type='http', auth="public", website=True)
-    def confirm_order(self, **post):
-        order = request.website.sale_get_order(context=request.context)
-        post['order'] = order
-        return super(website_sale, self).confirm_order(**post)
-
-    def checkout_values(self, data=None):
-        res = super(website_sale, self).checkout_values(data)
-        if data:
-            order = data.get('order')
-            if order:
-                res['order'] = order
-        return res
