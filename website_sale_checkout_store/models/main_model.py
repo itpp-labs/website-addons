be79eef7aff110066da190866f6a3c58766f7ece
Ilyas <rakhimkulov@it-projects.info>
2016-03-15 18:27:54 +0500

    settings

--- website_sale_checkout_store/models/main_model.py
+++ website_sale_checkout_store/models/main_model.py
@@ -2,7 +2,7 @@
 from openerp.osv import osv, orm
 from openerp.osv import fields as fields
 from openerp import models
-from openerp import fields as fields9
+from openerp import fields as fields_new_api
 class sale_order(osv.Model):
     _inherit = "sale.order"
     _columns = {
@@ -12,5 +12,5 @@ class sale_order(osv.Model):
 
 class website_config_settings(models.TransientModel):
     _inherit = 'website.config.settings'
-    nobill_noship = fields9.Boolean("Pickup and pay at store")
-    bill_noship = fields9.Boolean("Pickup at store but pay now")
+    module_nobill_noship = fields_new_api.Boolean("Pickup and pay at store")
+    module_bill_noship = fields_new_api.Boolean("Pickup at store but pay now")
