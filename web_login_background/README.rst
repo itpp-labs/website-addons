387b372938314debe01cffe5257c5e62235534ff
Ilyas <rakhimkulov@it-projects.info>
2016-03-18 13:24:03 +0500

    [IMP] Chlog, openerp to standart, README

--- web_login_background/README.rst
+++ web_login_background/README.rst
@@ -1,5 +1,7 @@
 Web Login Background
 ====================
 
-Tested on Odoo 8.0 a0797d3b35cc235048e7947dd7a3d38e18c3e350
+Set your background picture on odoo login and signup screens.
+
+Tested on Odoo 8.0 7b7f3fa76a822f05283e36b40bdbc58793f84570
 
--- web_login_background/__openerp__.py
+++ web_login_background/__openerp__.py
@@ -1,14 +1,27 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "Web login background",
-    'author': "IT-Projects LLC, Ildar Nasyrov",
-    'license': 'GPL-3',
-    'website': "https://twitter.com/nasyrov_ildar",
-    'category': 'Base',
-    'version': '1.0.0',
-    'depends': ['base'],
-    'data': [
+     "name": """Web login background""",
+     "summary": """Set your background for odoo login page""",
+     "category": "Base",
+     "images": [],
+     "version": "1.0.1",
+
+     "author": "IT-Projects LLC, Ildar Nasyrov",
+     "website": "https://it-projects.info",
+     "license": "GPL-3",
+     #"price": 9.00,
+     #"currency": "EUR",
+
+     "depends": [
+         "base",
+     ],
+     "external_dependencies": {"python": [], "bin": []},
+     "data": [
         'templates.xml',
         'views/attachment.xml',
-    ],
-}
+     ],
+     "demo": [
+     ],
+     "installable": True,
+     "auto_install": False,
+ }
