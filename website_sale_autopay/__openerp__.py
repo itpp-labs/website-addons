9865ce9931c41b6e3a02b3a8defc7bce1e0995ee
Ilyas <rakhimkulov@it-projects.info>
2016-04-21 12:28:35 +0500

    [DOC] price

--- website_sale_autopay/__openerp__.py
+++ website_sale_autopay/__openerp__.py
@@ -9,8 +9,8 @@
     "author": "IT-Projects LLC",
     "website": "https://it-projects.info",
     "license": "LGPL-3",
-    #"price": 9.00,
-    #"currency": "EUR",
+    "price": 90.00,
+    "currency": "EUR",
 
     "depends": [
         "website_sale",
