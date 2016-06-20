ee2ea85f677f4343c070663feafc5527ea9376c6
Ivan Yelizariev <yelizariev@it-projects.info>
2016-06-20 13:51:22 +0500

    [DOC] fix menu names and spelling

--- website_sale_add_to_cart_disable/doc/index.rst
+++ website_sale_add_to_cart_disable/doc/index.rst
@@ -5,6 +5,6 @@
 Usage
 =====
 
-* Open product form (e.g. via ``Sale / Product / Product``)
+* Open product form (e.g. via ``Sale / Products / Products``)
 * Switch ``[x] Show Add To Cart button`` off if needed
 * Open the product in eCommerce -- ``Add To Cart`` button is hidden
--- website_sale_add_to_cart_disable/product.py
+++ website_sale_add_to_cart_disable/product.py
@@ -4,4 +4,4 @@ from openerp import api, models, fields, SUPERUSER_ID
 class product_template(models.Model):
     _inherit = 'product.template'
 
-    sale_on_website = fields.Boolean('Show Add To Cart button', help='Switch off to disable sale on eCommerce', default=True)
+    sale_on_website = fields.Boolean('Show Add To Cart button', help='Switch off to disable sales via eCommerce', default=True)
