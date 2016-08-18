e418d856b9a791905682411c6f76c7d0e2a51a05
Ivan Yelizariev <yelizariev@it-projects.info>
2016-08-18 15:48:35 +0500

    [FIX] mark website_sales_team as non installable,
    because it depends on website_sale_order, which is not installable now.
    
    TODO: use website_sale_checkout_store instead

--- website_sales_team/__openerp__.py
+++ website_sales_team/__openerp__.py
@@ -23,5 +23,5 @@ Tested on Odoo 8.0 d023c079ed86468436f25da613bf486a4a17d625
         'website_sales_team_templates.xml',
         'website_sales_team_views.xml',
     ],
-    'installable': True
+    'installable': False,
 }
