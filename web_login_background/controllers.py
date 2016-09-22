427001308778fc83c564978a582b6d46bd889d9e
Ivan Yelizariev <yelizariev@it-projects.info>
2016-09-22 17:59:21 +0500

    [FIX] support for url attachments

--- web_login_background/controllers.py
+++ web_login_background/controllers.py
@@ -1,9 +1,16 @@
 # -*- coding: utf-8 -*-
+import hashlib
+
 from openerp import http
 from random import choice
 from openerp.addons.web.controllers.main import Home
 from openerp.http import request
 from openerp.addons.auth_signup.controllers.main import AuthSignupHome
+
+
+def _attachment2url(att):
+    sha = hashlib.sha1(getattr(att, '__last_update')).hexdigest()[0:7]
+    return '/web/image/%s-%s' % (att.id, sha)
 
 
 class Background(Home):
@@ -13,8 +20,8 @@ class Background(Home):
         pictures = request.env['ir.attachment'].search([
             ('use_as_background', '=', True)])
         if pictures:
-            picture_url = r'/web/image/' + \
-                          str(choice(pictures.mapped('id')))
+            p = choice(pictures)
+            picture_url = p.url or _attachment2url(p)
             request.params['picture_url'] = picture_url
 
         return super(Background, self).web_login(**kw)
@@ -27,7 +34,8 @@ class BackgroundSignup(AuthSignupHome):
         pictures = request.env['ir.attachment'].search([
             ('use_as_background', '=', True)])
         if pictures:
-            picture_url = r'/web/image/' + str(choice(pictures.mapped('id')))
+            p = choice(pictures)
+            picture_url = p.url or _attachment2url(p)
             request.params['picture_url'] = picture_url
 
         return super(BackgroundSignup, self).web_auth_signup(**kw)
