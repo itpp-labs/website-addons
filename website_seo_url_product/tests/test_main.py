6273e796ceb5fd71369fbab7950f6da32ea6c3d9
Ivan Yelizariev <yelizariev@it-projects.info>
2016-10-11 15:52:35 +0500

    [PORT] update tests

--- website_seo_url_product/tests/test_main.py
+++ website_seo_url_product/tests/test_main.py
@@ -7,15 +7,14 @@ class TestWebsiteSeoUrlProduct(TransactionCase):
 
     def setUp(self):
         super(TestWebsiteSeoUrlProduct, self).setUp()
-        cr, uid = self.cr, self.uid
-        self.ir_model_data = self.registry('ir.model.data')
-        self.usb_adapter_id = self.ir_model_data.get_object_reference(cr, uid, 'product', 'product_product_48')[1]
-        self.datacard_id = self.ir_model_data.get_object_reference(cr, uid, 'product', 'product_product_46')[1]
+        self.product1 = self.env.ref('product.product_product_1')
+        self.product2 = self.env.ref('product.product_product_2')
 
     def test_10_seo_url_checking(self):
-        cr, uid, context = self.cr, self.uid, {}
         SEO_URL_VALUE = 'some-seo-url'
 
         # should be no duplicated SEO URLs
-        self.registry('product.product').write(cr, uid, [self.usb_adapter_id, self.datacard_id], {'seo_url': SEO_URL_VALUE}, context=context)
-        self.assertEqual(1, self.registry('product.product').search_count(cr, uid, [('seo_url', '=', SEO_URL_VALUE)], context=context))
+        vals = {'seo_url': SEO_URL_VALUE}
+        self.product1.write(vals)
+        self.product2.write(vals)
+        self.assertEqual(1, self.env['product.product'].search_count([('seo_url', '=', SEO_URL_VALUE)]))
