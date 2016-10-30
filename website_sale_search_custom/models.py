d411d62d40549e66c134670d0336a6c108ffb49a
Ivan Yelizariev <yelizariev@it-projects.info>
2016-10-30 11:36:53 +0500

    [PORT] website_sale_search_custom

--- website_sale_search_custom/models.py
+++ website_sale_search_custom/models.py
@@ -1,9 +1,8 @@
 # -*- coding: utf-8 -*-
-from openerp import http
-from openerp import models
-from openerp.http import request
+from odoo import http, models, api
+from odoo.http import request
 
-from openerp.addons.website_sale.controllers.main import WebsiteSale as controller
+from odoo.addons.website_sale.controllers.main import WebsiteSale as controller
 
 
 class WebsiteSale(controller):
@@ -14,7 +13,9 @@ class WebsiteSale(controller):
                  '/shop/category/<model("product.public.category"):category>/page/<int:page>'
                  ], type='http', auth="public", website=True)
     def shop(self, page=0, category=None, search='', **post):
-        request.context['search_tags'] = search
+        new_context = request.context.copy()
+        new_context['search_tags'] = search
+        request.context = new_context
         if category and search:
             category = None
         return super(WebsiteSale, self).shop(page, category, search, **post)
@@ -23,16 +24,19 @@ class WebsiteSale(controller):
 class Product(models.Model):
     _inherit = 'product.template'
 
-    def _extend_domain(self, domain, context):
-        if not (context and context.get('search_tags')):
+    def _extend_domain(self, domain):
+        print '_extend_domain', self.env.context
+        if not (self.env.context.get('search_tags')):
             return domain
-        domain = ['|', ('tag_ids', 'ilike', context.get('search_tags'))] + domain
+        domain = ['|', ('tag_ids', 'ilike', self.env.context.get('search_tags'))] + domain
         return domain
 
-    def search_count(self, cr, uid, domain, context=None):
-        domain = self._extend_domain(domain, context)
-        return super(Product, self).search_count(cr, uid, domain, context=context)
+    @api.model
+    def search_count(self, domain):
+        domain = self._extend_domain(domain)
+        return super(Product, self).search_count(domain)
 
-    def search(self, cr, uid, domain, offset=0, limit=None, order=None, context=None, count=False):
-        domain = self._extend_domain(domain, context)
-        return super(Product, self).search(cr, uid, domain, offset=offset, limit=limit, order=order, context=context, count=count)
+    @api.model
+    def search(self, domain, offset=0, limit=None, order=None, count=False):
+        domain = self._extend_domain(domain)
+        return super(Product, self).search(domain, offset=offset, limit=limit, order=order, count=count)
