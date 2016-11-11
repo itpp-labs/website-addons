6d0bd2cac7f696e469cd35f2ceb889153e40b306
Jerome Boisvert-Chouinard <jerome.boisvertchouinard@savoirfairelinux.com>
2016-11-11 18:57:55 -0500

    split website_sale_search_custom into 2 modules
    the custom search module contained 2 functionalities: clearing the search on category change, and
    searching product tags. each functionality was split into a separate module (website_sale_search_tags
    and website_sale_search_clear)

---
+++ website_sale_search_clear/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+from . import controllers
+
+
+def post_load():
+    from . import query_url
---
+++ website_sale_search_clear/query_url.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+import werkzeug
+
+from openerp.addons.website_sale.controllers import main as main_file
+
+
+class QueryURL(object):
+
+    def __init__(self, path='', **args):
+        self.path = path
+        self.args = args
+
+    def __call__(self, path=None, **kw):
+        if not path:
+            path = self.path
+        is_category = path.startswith('/shop/category/')
+        for k, v in self.args.items():
+            if is_category and k == 'search':
+                continue
+            kw.setdefault(k, v)
+        l = []
+        for k, v in kw.items():
+            if v:
+                if isinstance(v, list) or isinstance(v, set):
+                    l.append(werkzeug.url_encode([(k, i) for i in v]))
+                else:
+                    l.append(werkzeug.url_encode([(k, v)]))
+        if l:
+            path += '?' + '&'.join(l)
+        return path
+
+main_file.QueryURL = QueryURL
