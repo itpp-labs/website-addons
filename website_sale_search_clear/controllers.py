94aed149fd39ba9a6dd6fcf5dcc44c6e4f2a09b9
Ivan Yelizariev <yelizariev@it-projects.info>
2017-01-18 10:40:32 +0500

    [PORT] fix imports

--- website_sale_search_clear/controllers.py
+++ website_sale_search_clear/controllers.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
-from openerp import http
-from openerp.addons.website_sale.controllers.main import website_sale as controller
+from odoo import http
+from odoo.addons.website_sale.controllers.main import WebsiteSale as controller
 
 
 class WebsiteSale(controller):
