27a9639f041ffe941aabcbdf2fd36d8d8aa78eb9
ArtyomLosev <losev@it-projects.info>
2017-03-27 15:05:13 +0500

    [FIX]fixed problem with displaying quantity on product page

--- website_sale_add_to_cart/static/src/js/website_sale_add_to_cart.js
+++ website_sale_add_to_cart/static/src/js/website_sale_add_to_cart.js
@@ -46,14 +46,14 @@ $(document).ready(function () {
     */
 
     if (page_product_id)
-        $('input.js_quantity').val(0);
+        $('input.form-control').val(0);
     ajax.jsonRpc("/shop/get_order_numbers", 'call').then(function(data){
         if (!data)
             return;
         $.each(data, function(product_id, num){
             if (page_product_id){
                 if (page_product_id == product_id){
-                    $('input.js_quantity').val(num);
+                    $('input.form-control').val(num);
                 }
             } else
                 $('input[data-product-id="'+product_id+'"]').val(num);
