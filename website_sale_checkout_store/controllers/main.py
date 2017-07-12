50d4a28b5e8f8e10484d7b66410b01cc15090751
GabbasovDinar <gabbasov@it-projects.info>
2017-07-12 14:37:47 +0500

    [REF] Refactoring code

--- website_sale_checkout_store/controllers/main.py
+++ website_sale_checkout_store/controllers/main.py
@@ -8,20 +8,32 @@ class WebsiteSaleExtended(WebsiteSale):
 
     @http.route(['/shop/checkout'], type='http', auth="public", website=True)
     def checkout(self, **post):
-        order = request.website.sale_get_order(force_create=1)
+        order = request.website.sale_get_order()
+
         redirection = self.checkout_redirection(order)
         if redirection:
             return redirection
+
         try:
             order.buy_way = post['buyMethod']
         except:
             pass
+
         if order.partner_id.id == request.website.user_id.sudo().partner_id.id:
             return request.redirect('/shop/address')
-        values = self.checkout_values()
+
+        for f in self._get_mandatory_billing_fields():
+            if not order.partner_id[f]:
+                return request.redirect('/shop/address?partner_id=%d' % order.partner_id.id)
+
+        values = self.checkout_values(**post)
         values['order'] = order
         sale_order_id = request.session.get('sale_order_id')
         request.env["sale.order"].browse(sale_order_id).sudo().payment_and_delivery_method_info()
+
+        # Avoid useless rendering if called in ajax
+        if post.get('xhr'):
+            return 'ok'
         return request.render("website_sale.checkout", values)
 
     @http.route(['/shop/payment'], type='http', auth="public", website=True)
