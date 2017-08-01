e1791e0e5ccfd01d2d63cad63d3ce4a77fd5f833
Your Name <kolushov@it-projects.info>
2017-08-01 17:39:06 +0500

    [LINT] deprecated-module, old-api7

--- website_sale_quantity_hide/models/sale_order.py
+++ website_sale_quantity_hide/models/sale_order.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
-from openerp.osv import osv
+from openerp import models, http
 
 
-class SaleOrder(osv.Model):
+class SaleOrder(models.Model):
     _inherit = "sale.order"
 
-    def _cart_update(self, cr, uid, ids, product_id=None, line_id=None, add_qty=0, set_qty=0, context=None, **kwargs):
-        product = self.pool['product.product'].browse(cr, uid, product_id)
+    def _cart_update(self, product_id=None, line_id=None, add_qty=0, set_qty=0):
+        product = http.request.env['product.product'].browse(product_id)
         if product.sale_one_only:
             add_qty = 0
             set_qty = 1
-        return super(SaleOrder, self)._cart_update(cr, uid, ids, product_id, line_id, add_qty, set_qty, context=context, **kwargs)
+        return super(SaleOrder, self)._cart_update(product_id, line_id, add_qty, set_qty)
