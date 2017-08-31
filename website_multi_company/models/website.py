08d745f2e454c5e9464de989e7aa0511fcb3e9ee
Ivan Yelizariev <yelizariev@it-projects.info>
2017-08-31 15:12:31 +0000

    [REF] use existing method instead of copy-pasting it

--- website_multi_company/models/website.py
+++ website_multi_company/models/website.py
@@ -10,9 +10,4 @@ class Website(models.Model):
     _inherit = "website"
 
     def multi_theme_reload(self):
-        """Update multiwebsite themes when loading a new wizard."""
-        _logger.info("Reloading available multi-website themes")
-        # Reload available single-website converted themes
-        self.env["website.theme"].search([])._convert_assets()
-        # Reload custom views for themes activated in any website
-        self.env["website"].search([])._multi_theme_activate()
+        self.env['website.config.settings'].multi_theme_reload()
