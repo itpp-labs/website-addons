d942be5ebaec5cf09916ff3af7dc0482f7cfaaa6
Your Name <kolushov@it-projects.info>
2017-10-31 18:58:45 +0500

    [FIX] in w_s_available for w_s_checkout_store

--- website_sale_available/controllers/website_sale_available.py
+++ website_sale_available/controllers/website_sale_available.py
@@ -12,12 +12,12 @@ class Controller(WebsiteSale):
 
     def checkout_redirection(self, order):
         res = super(Controller, self).checkout_redirection(order=order)
-        order = request.website.sale_get_order(context=request.context)
-
-        if not all([
-                line.product_uom_qty <= line.product_id.virtual_available
-                for line in order.order_line if not line.is_delivery
-        ]):
-            return request.redirect("/shop/cart")
+        # order = request.website.sale_get_order(context=request.context)
+        #
+        # if not all([
+        #         line.product_uom_qty <= line.product_id.virtual_available
+        #         for line in order.order_line if not line.is_delivery
+        # ]):
+        #     return request.redirect("/shop/cart")
 
         return res
