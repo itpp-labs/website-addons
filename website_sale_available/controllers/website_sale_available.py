d4d28cc0bb6954d656e6f33667ad4b96718245ab
Your Name <kolushov@it-projects.info>
2017-11-01 13:29:45 +0500

    [FIX] w_s_available context and is_delivery, [DOC] added index, changelog

--- website_sale_available/controllers/website_sale_available.py
+++ website_sale_available/controllers/website_sale_available.py
@@ -12,12 +12,14 @@ class Controller(WebsiteSale):
 
     def checkout_redirection(self, order):
         res = super(Controller, self).checkout_redirection(order=order)
-        # order = request.website.sale_get_order(context=request.context)
-        #
-        # if not all([
-        #         line.product_uom_qty <= line.product_id.virtual_available
-        #         for line in order.order_line if not line.is_delivery
-        # ]):
-        #     return request.redirect("/shop/cart")
+        order = request.website.with_context(request.context).sale_get_order()
+        for line in order.order_line:
+            print line.is_delivery
+
+        if not all([
+                line.product_uom_qty <= line.product_id.virtual_available
+                for line in order.order_line if not line.is_delivery
+        ]):
+            return request.redirect("/shop/cart")
 
         return res
---
+++ website_sale_available/doc/index.rst
@@ -0,0 +1,8 @@
+=========================================
+ Sale only available products on Website
+=========================================
+
+Installation
+============
+
+* `Install <https://odoo-development.readthedocs.io/en/latest/odoo/usage/install-module.html>`__ this module in a usual way
