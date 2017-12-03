9ad629881fcad2f17ad5b15cc3cbe1ffd5044b19
Ivan Yelizariev <yelizariev@it-projects.info>
2017-12-03 17:05:57 +0000

    [DEV] module template

---
+++ website_event_refund/__manifest__.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+{
+    "name": """Product swap at eCommerce""",
+    "summary": """{SHORT_DESCRIPTION_OF_THE_MODULE}""",
+    "category": "eCommerce",
+    # "live_test_url": "",
+    "images": [],
+    "version": "1.0.0",
+    "application": False,
+
+    "author": "IT-Projects LLC, Ivan Yelizariev",
+    "support": "apps@it-projects.info",
+    "website": "https://it-projects.info/team/yelizariev",
+    "license": "LGPL-3",
+    # "price": 9.00,
+    # "currency": "EUR",
+
+    "depends": [
+        "{DEPENDENCY1}",
+        "{DEPENDENCY2}",
+    ],
+    "external_dependencies": {"python": [], "bin": []},
+    "data": [
+        "{FILE1}.xml",
+        "{FILE2}.xml",
+    ],
+    "qweb": [
+        "static/src/xml/{QWEBFILE1}.xml",
+    ],
+    "demo": [
+        "demo/{DEMOFILE1}.xml",
+    ],
+
+    "post_load": None,
+    "pre_init_hook": None,
+    "post_init_hook": None,
+    "uninstall_hook": None,
+
+    "auto_install": False,
+    "installable": True,
+}
