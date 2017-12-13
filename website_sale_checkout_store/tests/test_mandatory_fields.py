89b977d2f2540ddac12522a6780bfcd0da267fac
Your Name <kolushov@it-projects.info>
2017-12-13 16:54:16 +0500

    [CI] increased timout

--- website_sale_checkout_store/tests/test_mandatory_fields.py
+++ website_sale_checkout_store/tests/test_mandatory_fields.py
@@ -7,21 +7,21 @@ import odoo.tests
 class TestUi(odoo.tests.HttpCase):
 
     def test_checkout_nobill_noship(self):
-        self.phantom_js("/", "odoo.__DEBUG__.services['web_tour.tour'].run('shop_mandatory_fields_nobill_noship')",
+        self.phantom_js("/", "odoo.__DEBUG__.services['web_tour.tour'].run('shop_mandatory_fields_nobill_noship', 1000)",
                         "odoo.__DEBUG__.services['web_tour.tour'].tours.shop_mandatory_fields_nobill_noship.ready",
                         login=None)
 
     def test_checkout_bill_noship(self):
-        self.phantom_js("/", "odoo.__DEBUG__.services['web_tour.tour'].run('shop_mandatory_fields_bill_noship')",
+        self.phantom_js("/", "odoo.__DEBUG__.services['web_tour.tour'].run('shop_mandatory_fields_bill_noship', 1000)",
                         "odoo.__DEBUG__.services['web_tour.tour'].tours.shop_mandatory_fields_bill_noship.ready",
                         login=None)
 
     def test_checkout_bill_ship(self):
-        self.phantom_js("/", "odoo.__DEBUG__.services['web_tour.tour'].run('shop_mandatory_fields_bill_ship')",
+        self.phantom_js("/", "odoo.__DEBUG__.services['web_tour.tour'].run('shop_mandatory_fields_bill_ship', 1000)",
                         "odoo.__DEBUG__.services['web_tour.tour'].tours.shop_mandatory_fields_bill_ship.ready",
                         login=None)
 
     def test_checkout_nobill_ship(self):
-        self.phantom_js("/", "odoo.__DEBUG__.services['web_tour.tour'].run('shop_mandatory_fields_nobill_ship')",
+        self.phantom_js("/", "odoo.__DEBUG__.services['web_tour.tour'].run('shop_mandatory_fields_nobill_ship', 1000)",
                         "odoo.__DEBUG__.services['web_tour.tour'].tours.shop_mandatory_fields_nobill_ship.ready",
                         login=None)
