c7b5ec18581a40c65f3da43436703f7e82e8f391
Your Name <kolushov@it-projects.info>
2017-12-20 11:54:56 +0500

    [CI] increased timeouts

--- website_sale_checkout_store/tests/test_mandatory_fields.py
+++ website_sale_checkout_store/tests/test_mandatory_fields.py
@@ -5,22 +5,27 @@ import odoo.tests
 @odoo.tests.common.post_install(True)
 class TestUi(odoo.tests.HttpCase):
 
+    # big timeout due to long redirects (checkout -> confirmation) in nobill cases
     def test_checkout_nobill_noship(self):
-        self.phantom_js("/", "odoo.__DEBUG__.services['web_tour.tour'].run('shop_mandatory_fields_nobill_noship')",
+        self.phantom_js("/",
+                        "odoo.__DEBUG__.services['web_tour.tour'].run('shop_mandatory_fields_nobill_noship', 10000)",
                         "odoo.__DEBUG__.services['web_tour.tour'].tours.shop_mandatory_fields_nobill_noship.ready",
-                        login=None, timeout=100)
+                        login=None, timeout=160)
 
     def test_checkout_bill_noship(self):
-        self.phantom_js("/", "odoo.__DEBUG__.services['web_tour.tour'].run('shop_mandatory_fields_bill_noship')",
+        self.phantom_js("/",
+                        "odoo.__DEBUG__.services['web_tour.tour'].run('shop_mandatory_fields_bill_noship', 3000)",
                         "odoo.__DEBUG__.services['web_tour.tour'].tours.shop_mandatory_fields_bill_noship.ready",
-                        login=None, timeout=100)
+                        login=None, timeout=170)
 
     def test_checkout_bill_ship(self):
-        self.phantom_js("/", "odoo.__DEBUG__.services['web_tour.tour'].run('shop_mandatory_fields_bill_ship')",
+        self.phantom_js("/",
+                        "odoo.__DEBUG__.services['web_tour.tour'].run('shop_mandatory_fields_bill_ship', 3000)",
                         "odoo.__DEBUG__.services['web_tour.tour'].tours.shop_mandatory_fields_bill_ship.ready",
-                        login=None, timeout=100)
+                        login=None, timeout=200)
 
     def test_checkout_nobill_ship(self):
-        self.phantom_js("/", "odoo.__DEBUG__.services['web_tour.tour'].run('shop_mandatory_fields_nobill_ship')",
+        self.phantom_js("/",
+                        "odoo.__DEBUG__.services['web_tour.tour'].run('shop_mandatory_fields_nobill_ship', 10000)",
                         "odoo.__DEBUG__.services['web_tour.tour'].tours.shop_mandatory_fields_nobill_ship.ready",
-                        login=None, timeout=100)
+                        login=None, timeout=190)
