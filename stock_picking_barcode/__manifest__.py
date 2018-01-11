14ed1c3331dc8d3f91b285c121f82b74845a8640
ArtyomLosev <losev@it-projects.info>
2018-01-11 14:16:29 +0500

    [TEXT] version info update

--- stock_picking_barcode/__manifest__.py
+++ stock_picking_barcode/__manifest__.py
@@ -4,7 +4,7 @@
     "summary": """The module allows you to process Pickings by barcode scanner via special page /barcode/web (the same as it was in odoo 8.0)""",
     "category": "Warehouse",
     "images": [],
-    "version": "1.0.2",
+    "version": "1.0.3",
 
     "author": "IT-Projects LLC, Pavel Romanchenko",
     "support": "apps@it-projects.info",
