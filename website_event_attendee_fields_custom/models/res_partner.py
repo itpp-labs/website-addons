25c9d9ecd457635492c028f278a3197959c85904
Ivan Yelizariev <yelizariev@it-projects.info>
2018-01-14 18:20:43 +0500

    [IMP] rename field Passport to Passport Number

--- website_event_attendee_fields_custom/models/res_partner.py
+++ website_event_attendee_fields_custom/models/res_partner.py
@@ -6,6 +6,7 @@ class Partner(models.Model):
     _inherit = "res.partner"
 
     passport = fields.Char(
+        string='Passport Number',
         compute=lambda s: s._compute_identification(
             'passport', 'passport',
         ),
