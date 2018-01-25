9cb678cd29636363da89b5307bd77cf0cc6dab7d
Ivan Yelizariev <yelizariev@it-projects.info>
2018-01-25 08:41:05 +0000

    [REF] move switch_multi_company to website_multi_company

---
+++ website_multi_company/models/res_users.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from odoo import models, api
+
+
+class ResUsers(models.Model):
+    _inherit = 'res.users'
+
+    @api.multi
+    def switch_multi_company(self, company):
+        """
+        :returns: Is company set to new value
+        """
+        self.ensure_one()
+
+        user = self
+        if user.company_id == company:
+            return True
+
+        update_company = True
+        if company in user.company_ids:
+            pass
+        elif user.has_group('base.group_user'):
+            # User is internal and doesn't have access to that company
+            update_company = False
+        else:
+            # User is a portal user -- update his allowed companies
+            user.write({
+                'company_ids': [(4, company.id, 0)]
+            })
+
+        if update_company:
+            user.company_id = company
+
+        return update_company
--- website_multi_company_portal/controllers/main.py
+++ website_multi_company_portal/controllers/main.py
@@ -8,20 +8,5 @@ class MultiCompanyPortal(website_account):
     def _prepare_portal_layout_values(self):
         company = request.website.company_id
         user = request.env.user
-        if user.company_id != company:
-            update_company = True
-            if company in user.company_ids:
-                pass
-            elif user.has_group('base.group_user'):
-                # User is internal and doesn't have access to that company
-                update_company = False
-            else:
-                # User is a portal user -- update his allowed companie
-                user.write({
-                    'company_ids': [(4, company.id, 0)]
-                })
-
-            if update_company:
-                user.company_id = company
-
+        user.switch_multi_company(company)
         return super(MultiCompanyPortal, self)._prepare_portal_layout_values()
