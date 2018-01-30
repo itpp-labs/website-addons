7709a049702cbf1f71896fe1f82e868fa1a4d51b
Ivan Yelizariev <yelizariev@it-projects.info>
2018-01-30 14:10:30 +0000

    [CI] fix tour: next page may be /shop/address

--- website_event_attendee_fields/static/src/js/test_tour.js
+++ website_event_attendee_fields/static/src/js/test_tour.js
@@ -51,7 +51,7 @@ odoo.define('website_event_attendee_fields.test_tour', function (require) {
         },
         {
             content: "Dummy step to finish loadding of previous step",
-            trigger: 'h3:contains(We are glad to confirm your registration to our event),a:contains(Return to Cart)',
+            trigger: 'h3:contains(We are glad to confirm your registration to our event),a:contains(Return to Cart),h3:contains(Your Address)',
             run: function(){
                 // it's needed to don't make a click on the link
             },
