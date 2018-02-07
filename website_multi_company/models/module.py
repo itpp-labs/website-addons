fad1e15f53f1e27eb1c14ebf307391b30af6c17c
ILMIR <karamov@it-projects.info>
2018-02-07 14:06:50 +0500

    [ADD] website_multi_company: additional base modules (#285)

--- website_multi_company/models/module.py
+++ website_multi_company/models/module.py
@@ -1,6 +1,6 @@
 from odoo import models, api
 
-BASE_MODULES = ['website', 'website_sale', 'website_blog', 'website_sale_comparison', 'website_sale_wishlist', 'mass_mailing', 'web_settings_dashboard']
+BASE_MODULES = ['website_blog', 'website_sale_comparison', 'website_sale_wishlist', 'web_settings_dashboard', 'website_crm', 'website_animate', 'website_mass_mailing', 'contacts', 'fetchmail', 'resource', 'calendar', 'snippet_google_map']
 
 
 class Module(models.Model):
