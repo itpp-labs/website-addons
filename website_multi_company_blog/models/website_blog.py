a4cdab162982bed72d70c42f4bd6c11d2984c7be
ILMIR <karamov@it-projects.info>
2018-03-01 18:00:07 +0500

    [ADD] help attribute

--- website_multi_company_blog/models/website_blog.py
+++ website_multi_company_blog/models/website_blog.py
@@ -6,4 +6,4 @@ from odoo import models, fields
 class Blog(models.Model):
     _inherit = 'blog.blog'
 
-    website_ids = fields.Many2many('website', string='Allowed websites')
+    website_ids = fields.Many2many('website', string='Allowed websites', help='Set the websites your blog should be available on. Leave empty to post the blog on each website you created.')
