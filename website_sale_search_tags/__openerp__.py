90e94561812f07f5761c8fe4dfbac0b37ee8c0e3
tslai <tl@quartile.co>
2018-03-20 02:43:51 +0000

    [FIX] shop() param

--- website_sale_search_tags/__openerp__.py
+++ website_sale_search_tags/__openerp__.py
@@ -3,9 +3,10 @@
     'name': "Website Search Product Tags",
     'summary': """Search website products by tags""",
     'category': 'eCommerce',
-    'version': '1.0.0',
+    'version': '1.1.0',
     'application': False,
-    'author': 'IT-Projects LLC, Ivan Yelizariev, Savoir-faire Linux',
+    'author': 'IT-Projects LLC, Ivan Yelizariev, Savoir-faire Linux, '
+              'Quartile Limited Tim Lai',
     'license': 'GPL-3',
     "support": "apps@it-projects.info",
     'website': 'https://twitter.com/yelizariev',
