c1f731be8c310d7043ea92659daffd14c42ce5f4
Ilmir <ilmir.karamov@gmail.com>
2018-03-28 14:44:54 +0500

    [FIX] website_sale_search_tags: error 500 in shop page

--- website_sale_search_tags/controllers.py
+++ website_sale_search_tags/controllers.py
@@ -8,6 +8,6 @@ from odoo.addons.website_sale.controllers.main import WebsiteSale as controller
 class WebsiteSale(controller):
 
     @http.route()
-    def shop(self, page=0, category=None, search='', **post):
+    def shop(self, page=0, category=None, search='', ppg=False, **post):
         request.context = dict(request.context, search_tags=search)
-        return super(WebsiteSale, self).shop(page, category, search, **post)
+        return super(WebsiteSale, self).shop(page, category, search, ppg, **post)
