ba59002afc22131fa861335097333283174c1903
ILMIR <karamov@it-projects.info>
2018-03-28 14:23:29 +0500

    [DOC] website_sale_search_clear: new version in manifest file

--- website_sale_search_clear/__manifest__.py
+++ website_sale_search_clear/__manifest__.py
@@ -3,7 +3,7 @@
     'name': "Website Sale Search Clear",
     'summary': """Clear the search""",
     'category': 'eCommerce',
-    'version': '1.1.0',
+    'version': '11.0.1.0.1',
     'application': False,
     'author': 'IT-Projects LLC, Ivan Yelizariev, Savoir-faire Linux, Nicolas JEUDY, Quartile Limited Tim Lai',
     'license': 'GPL-3',
