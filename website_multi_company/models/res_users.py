0fba15d438c257feab7ebc463cb09ab6ccaa6c95
Ildar Nasyrov <nasyrov@it-projects.info>
2018-03-29 11:52:42 +0000

    [ADD] website_multi_company: domain for editor_website_ids - could only be websites that belong to company from allowed list

--- website_multi_company/models/res_users.py
+++ website_multi_company/models/res_users.py
@@ -1,11 +1,23 @@
 # -*- coding: utf-8 -*-
-from odoo import models, fields, api
+from odoo import api, fields, models, _
+from odoo.exceptions import ValidationError
 
 
 class ResUsers(models.Model):
     _inherit = 'res.users'
 
     editor_website_ids = fields.Many2many('website', string='Editor on websites', help='Empty list allows edit any website')
+
+    @api.onchange('company_ids')
+    def _onchange_company_ids(self):
+        return self.company_ids and {'domain': {'editor_website_ids': [('company_id', 'in', self.company_ids.ids)]}} or {'domain': {'editor_website_ids': []}}
+
+    @api.constrains('company_ids', 'editor_website_ids')
+    def _check_websites_in_companies(self):
+        for record in self:
+            websites_companies = record.editor_website_ids.mapped('company_id')
+            if record.company_ids and record.editor_website_ids and not websites_companies <= record.company_ids:
+                raise ValidationError(_("Error! You can select as editable only the allowed companies's websites - check the 'Editor on websites' field in preferences"))
 
     @api.multi
     def switch_multi_company(self, company):
