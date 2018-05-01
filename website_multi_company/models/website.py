af151edfbe2bb6dddf198f53ac7868a596b20c58
Ivan Yelizariev <yelizariev@it-projects.info>
2018-05-02 02:58:43 +0500

    [IMP] sub-theme support
    see website_multi_theme/README for understanding for sub-themes

--- website_multi_company/models/website.py
+++ website_multi_company/models/website.py
@@ -16,13 +16,19 @@ class Website(models.Model):
     def multi_theme_reload(self):
         self.ensure_one()
 
-        # Find theme views
-        self.multi_theme_id._convert_assets()
-
-        # Update views for current website
+        # convert_assets and copy views for current website
         self._multi_theme_activate()
 
     @api.multi
     def multi_theme_reload_list(self):
         # only reloads list
         self.env["website.theme"].search([])._convert_assets()
+
+    @api.multi
+    def _multi_theme_activate(self):
+        if not self.env.context.get('skip_converting_assets'):
+            # reload dependencies before activating
+            self.mapped('multi_theme_id')\
+                .upstream_dependencies()\
+                ._convert_assets()
+        return super(Website, self)._multi_theme_activate()
