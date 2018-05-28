93037f9864f2ec5c34679e9d0ec718d4f16c3069
Ivan Yelizariev <yelizariev@it-projects.info>
2018-05-29 02:43:17 +0500

    [FIX] demo tool uses slugify which translate all underscore to dash

--- website_event_attendee_signup/__manifest__.py
+++ website_event_attendee_signup/__manifest__.py
@@ -3,7 +3,7 @@
     "name": """Sign-up event attendees to portal""",
     "summary": """Attendees can use portal dashboard to make extra purchases for the event, for example.""",
     "category": "Marketing",
-    "live_test_url": "http://apps.it-projects.info/shop/product/portal_event_tickets?version=10.0",
+    "live_test_url": "http://apps.it-projects.info/shop/product/portal-event-tickets?version=10.0",
     "images": ["images/banner.jpg"],
     "version": "1.0.0",
     "application": False,
