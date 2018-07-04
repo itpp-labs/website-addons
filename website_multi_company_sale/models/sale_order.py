58139ce764f5e78885c199c4b8872f621b2cb005
Ildar <nasyrov@it-projects.info>
2018-07-04 06:41:07 +0500

    :zap: real multi-website eCommerce (#335)
    +- **Add:** Users with ``Current Backend Website`` specified have access only to sale orders from that website
    +- **Fix:** When use is logged in on different websites, he can use shopping carts on each websites independently 
    +- **Improvement:** group sale orders by website

--- website_multi_company_sale/models/sale_order.py
+++ website_multi_company_sale/models/sale_order.py
@@ -10,16 +10,27 @@ class Website(models.Model):
     def sale_get_order(self, force_create=False, code=None, update_pricelist=False, force_pricelist=False):
         company = request.website.company_id
         if not request.session.get('sale_order_id'):
-            # original sale_get_order uses last_website_so_id only when there is
+            # original sale_get_order uses last_website_so_id only when there is no
             # sale_order_id in the session
 
             # company.id seems to be the same as self.id, but let's use variant
             # from original sale_get_order
             self = self.with_context(force_company=company.id)
-        return super(Website, self).sale_get_order(force_create, code, update_pricelist, force_pricelist)
+        sale_order = super(Website, self).sale_get_order(force_create, code, update_pricelist, force_pricelist)
+
+        if sale_order and force_create:
+            sale_order.website_id = self.id
+
+        return sale_order
 
 
 class ResPartner(models.Model):
     _inherit = 'res.partner'
 
-    last_website_so_id = fields.Many2one(company_dependent=True)
+    last_website_so_id = fields.Many2one(company_dependent=True, website_dependent=True)
+
+
+class SaleOrder(models.Model):
+    _inherit = "sale.order"
+
+    website_id = fields.Many2one('website', 'Online Order Website', readonly=True)
---
+++ website_multi_company_sale/tests/test_sale_get_order.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+import urllib
+
+import odoo.tests
+from odoo.tests.common import PORT, HttpCase, get_db_name
+from odoo import api
+
+
+@odoo.tests.common.at_install(True)
+@odoo.tests.common.post_install(True)
+class TestSaleGetOrder(HttpCase):
+
+    def setUp(self):
+        super(TestSaleGetOrder, self).setUp()
+
+    def test_sale_get_order(self):
+        phantom_env = api.Environment(self.registry.test_cr, self.uid, {})
+
+        website1 = phantom_env.ref('website.default_website')
+        website1.domain = '127.0.0.1'
+        website2 = phantom_env.ref('website.website2')
+        website2.domain = 'localhost'
+
+        product_template = phantom_env.ref('product.product_product_11_product_template')
+        product_attribute = phantom_env.ref('product.product_attribute_1')
+        product_attribute_value = phantom_env.ref('product.product_attribute_value_1')
+        attribute = 'attribute-%s-%s' % (product_template.id, product_attribute.id)
+        form_data = {
+            'product_id': product_template.id,
+            attribute: product_attribute_value.id,
+            'add_qty': 1,
+        }
+        data = urllib.urlencode(form_data)
+
+        login = "demo"
+        self.authenticate(login, login)
+
+        count_so_before = phantom_env['sale.order'].sudo().search_count([])
+
+        response = self.url_open("http://127.0.0.1:%d/shop/cart/update" % PORT, data=data, timeout=60)
+        self.assertEqual(response.getcode(), 200)
+        so_last = phantom_env['sale.order'].search([], limit=1)
+        self.assertEqual(so_last.website_id, website1)
+
+        # setup a magic session_id that will be rollbacked
+        self.session = odoo.http.root.session_store.new()
+        self.session_id = self.session.sid
+        self.session.db = get_db_name()
+        odoo.http.root.session_store.save(self.session)
+        self.authenticate(login, login)
+
+        headers = dict(self.opener.addheaders)
+        headers['Cookie'] = 'session_id=%s' % self.session_id
+        self.opener.addheaders = headers.items()
+
+        response = self.url_open("http://localhost:%d/shop/cart/update" % PORT, data=data, timeout=60)
+        self.assertEqual(response.getcode(), 200)
+        so_last = phantom_env['sale.order'].search([], limit=1)
+        self.assertEqual(so_last.website_id, website2)
+
+        count_so_after = phantom_env['sale.order'].sudo().search_count([])
+        self.assertEqual(count_so_after, count_so_before+2)
