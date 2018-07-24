ed6901a9f03a2b8806076ee9505351397c999a6c
Ildar Nasyrov <nasyrov@it-projects.info>
2018-07-24 08:14:26 +0000

    :ambulance: website_multi_company: ValueError: Field `editor_website_ids` does not exist

--- website_multi_company/models/res_users.py
+++ website_multi_company/models/res_users.py
@@ -1,4 +1,26 @@
-from odoo import models, api
+# Copyright 2018 Ivan Yelizariev <https://it-projects.info/team/yelizariev>
+# Copyright 2018 Ildar Nasyrov <https://it-projects.info/team/iledarn>
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+
+from odoo import api, fields, models, _
+from odoo.exceptions import ValidationError
+
+
+class ResUsers(models.Model):
+    _inherit = 'res.users'
+
+    editor_website_ids = fields.Many2many('website', string='Editor on websites', help='Empty list allows edit any website')
+
+    @api.onchange('company_ids')
+    def _onchange_company_ids(self):
+        return self.company_ids and {'domain': {'editor_website_ids': [('company_id', 'in', self.company_ids.ids)]}} or {'domain': {'editor_website_ids': []}}
+
+    @api.constrains('company_ids', 'editor_website_ids')
+    def _check_websites_in_companies(self):
+        for record in self:
+            websites_companies = record.editor_website_ids.mapped('company_id')
+            if record.company_ids and record.editor_website_ids and not websites_companies <= record.company_ids:
+                raise ValidationError(_("Error! You can select as editable only the allowed companies's websites - check the 'Editor on websites' field in preferences"))
 
 
 class ResUsers(models.Model):
