cfe5023f4e9486dfae27cb7f0c88711730317154
Ivan Yelizariev <yelizariev@it-projects.info>
2018-08-17 22:02:49 +0500

    :bomb: debug manually frontend + backend

--- website_multi_company/static/src/js/web_editor.js
+++ website_multi_company/static/src/js/web_editor.js
@@ -19,23 +19,26 @@ odoo.define('website_multi_company.web_editor', function (require) {
         },
         _displayResource: function (resID, type) {
             this._super.apply(this, arguments);
-            // button is visible in xml mode when debug is active and view is not multi-website yet
+            // Not a good way, but otherwise we have to override method
+            // get_assets_editor_resources, which is but to keep system
+            // extendable
             var is_multi = this.resources['xml'][resID].name.indexOf('(Website #') !== -1;
+            // button is visible in xml mode when debug is active and view is not multi-website yet
             this.$makeMultiWebsite.toggleClass('hidden', is_multi || this.currentType === 'less' || !session.debug);
         },
         _onMakeMultiWebsiteClick: function(){
             var resID = this._getSelectedResource();
+            this._makeMultiWebsite(resID);
         },
         _makeMultiWebsite: function(resID){
-            var esession = this.editingSessions['xml'][resID];
             this._rpc({
                 model: 'ir.ui.view',
                 method: 'make_multi_website',
-                args: [[esession.id], weContext],
-            }).then(function(){
+                args: [[resID], weContext],
+            }).then((function(){
                 // save other changes and reload page
                 return this._saveResources();
-            }).bind(this);
+            }).bind(this));
         },
     });
     return MultiViewEditor;
