62762cdc954f51f5ef73ea37e26da14c17943bb8
Ivan Yelizariev <yelizariev@it-projects.info>
2018-08-20 17:42:09 +0500

    :fire_engine: lead creation tests

--- website_multi_company_crm/models/crm_lead.py
+++ website_multi_company_crm/models/crm_lead.py
@@ -11,7 +11,11 @@ _logger = logging.getLogger(__name__)
 class Lead(models.Model):
     _inherit = 'crm.lead'
 
-    website_id = fields.Many2one('website', string='From Website')
+    def _default_website_id(self):
+        return self.env.context.get('website_id') or self.env.user.backend_website_id.id
+
+    website_id = fields.Many2one('website', string='From Website', default=_default_website_id)
+    company_id = fields.Many2one(default=lambda self: self.env['res.company']._company_default_get())
 
     @api.onchange('company_id')
     def _onchange_company_id(self):
@@ -28,5 +32,5 @@ class Lead(models.Model):
     @api.constrains('company_id', 'website_id')
     def _check_website_in_company(self):
         for record in self:
-            if record.company_id and record.website_id and not record.website_id.company_id <= record.company_id:
+            if record.company_id and record.website_id and not record.website_id.company_id == record.company_id:
                 raise ValidationError(_("Error! Website and Company are mismatched"))
