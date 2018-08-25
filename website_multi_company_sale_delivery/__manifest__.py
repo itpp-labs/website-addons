47c3128155313831468a92c5de945ef52bdb284e
Ildar <nasyrov@it-projects.info>
2018-08-25 21:13:43 +0500

    :ambulance: shipping countries should comply with current websites's delivery_carriers countryes (#363)

--- website_multi_company_sale_delivery/__manifest__.py
+++ website_multi_company_sale_delivery/__manifest__.py
@@ -7,7 +7,7 @@
     "category": "eCommerce",
     "live_test_url": "http://apps.it-projects.info/shop/product/website-multi-company?version=10.0",
     "images": ["images/website_multi_company_sale_delivery_main.png"],
-    "version": "10.0.1.0.0",
+    "version": "10.0.1.0.1",
     "application": False,
 
     "author": "IT-Projects LLC, Ildar Nasyrov",
--- website_multi_company_sale_delivery/models/__init__.py
+++ website_multi_company_sale_delivery/models/__init__.py
@@ -1,3 +1,4 @@
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 from . import delivery_carrier
 from . import sale_order
+from . import res_country
