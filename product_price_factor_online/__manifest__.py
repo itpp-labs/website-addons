ccca74ca32c225c2aaa1e11001adf54f71db267c
KolushovAlexandr <kolushov@it-projects.info>
2018-12-25 13:28:20 +0500

    :book: travis error due to lack of version in the module

--- product_price_factor_online/__manifest__.py
+++ product_price_factor_online/__manifest__.py
@@ -10,6 +10,7 @@
     'images': ['images/1.png'],
     "support": "apps@it-projects.info",
     'website': "https://twitter.com/nasyrov_ildar",
+    'version': '11.0.1.0.0',
     'depends': ['website_sale', 'product_price_factor'],
     'data': [
         'views/templates.xml',
