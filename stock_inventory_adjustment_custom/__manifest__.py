dc620ab215a0a4df4e69893b7c8062048c3e5d28
KolushovAlexandr <kolushov@it-projects.info>
2019-01-31 14:32:22 +0500

    :tada: stock_inventory_adjustment_custom

---
+++ stock_inventory_adjustment_custom/__manifest__.py
@@ -0,0 +1,53 @@
+# Copyright 2019 Kolushov Alexandr <https://it-projects.info/team/KolushovAlexandr>
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+{
+    "name": """Inventory Adjustment Customization""",
+    "summary": """Inventory Adjustment Customization""",
+    "category": "Warehouse",
+    # "live_test_url": "http://apps.it-projects.info/shop/product/DEMO-URL?version=11.0",
+    "images": [],
+    "version": "11.0.1.0.0",
+    "application": False,
+
+    "author": "IT-Projects LLC, Kolushov Alexandr",
+    "support": "apps@it-projects.info",
+    "website": "https://it-projects.info/team/KolushovAlexandr",
+    "license": "LGPL-3",
+    # "price": 9.00,
+    # "currency": "EUR",
+
+    "depends": [
+        "{DEPENDENCY1}",
+        "{DEPENDENCY2}",
+    ],
+    "external_dependencies": {"python": [], "bin": []},
+    "data": [
+        "{FILE1}.xml",
+        "{FILE2}.xml",
+    ],
+    "demo": [
+        "demo/{DEMOFILE1}.xml",
+    ],
+    "qweb": [
+        "static/src/xml/{QWEBFILE1}.xml",
+    ],
+
+    "post_load": None,
+    "pre_init_hook": None,
+    "post_init_hook": None,
+    "uninstall_hook": None,
+
+    "auto_install": False,
+    "installable": True,
+
+    # "demo_title": "Inventory Adjustment Customization",
+    # "demo_addons": [
+    # ],
+    # "demo_addons_hidden": [
+    # ],
+    # "demo_url": "DEMO-URL",
+    # "demo_summary": "Inventory Adjustment Customization",
+    # "demo_images": [
+    #    "images/MAIN_IMAGE",
+    # ]
+}
