0b160a56f5de05eded1fdcf31febf97645a336fe
ruzilia <m_ruzilya@inbox.ru>
2019-07-30 10:53:16 +0200

    :book: image for Appstore added

--- website_debranding/__manifest__.py
+++ website_debranding/__manifest__.py
@@ -4,6 +4,7 @@
     'author': 'IT-Projects LLC, Ivan Yelizariev',
     'license': 'LGPL-3',
     'category': 'Debranding',
+    'images': ['images/website_debranding.png'],
     "support": "apps@it-projects.info",
     'website': 'https://twitter.com/yelizariev',
     # 'price': 60.00,
