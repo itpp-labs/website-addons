e6e95a0c7993808c73208d7b2b3252f159bab76a
ommo73 <rafailov@it-projects.info>
2019-08-16 14:44:07 +0500

    :book: too long module name

--- website_multi_company_sale/__manifest__.py
+++ website_multi_company_sale/__manifest__.py
@@ -1,5 +1,5 @@
 {
-    "name": """Real Multi Website (eCommerce extension)""",
+    "name": """eCommerce Support""",
     "summary": """Multi Website support in eCommerce""",
     "category": "eCommerce",
     # "live_test_url": "http://apps.it-projects.info/shop/product/website-multi-company?version=12.0",
