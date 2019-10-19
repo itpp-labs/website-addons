b49f72a98a101635ed1dfa91c8d47ef906c40c5f
Artur Nigmatullin <nigmatullin@it-projects.info>
2019-10-19 21:04:22 +0500

    :euro:

--- website_seo_url/__openerp__.py
+++ website_seo_url/__openerp__.py
@@ -9,7 +9,7 @@
     "support": "apps@it-projects.info",
     "website": "https://it-projects.info",
     "license": "LGPL-3",
-    "price": 110.00,
+    "price": 55.00,
     "currency": "EUR",
 
     "depends": [
