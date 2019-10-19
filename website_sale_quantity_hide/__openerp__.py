61073e53054ab02b54681682c71de73199c61f4e
Artur Nigmatullin <nigmatullin@it-projects.info>
2019-10-19 21:03:15 +0500

    :euro:

--- website_sale_quantity_hide/__openerp__.py
+++ website_sale_quantity_hide/__openerp__.py
@@ -3,7 +3,7 @@
     'summary': "Allows to sale only 1 item at once",
     'author': 'IT-Projects LLC, Ivan Yelizariev',
     'license': 'GPL-3',
-    "price": 20.00,
+    "price": 10.00,
     "currency": "EUR",
     'category': 'Website',
     'website': 'https://twitter.com/yelizariev',
