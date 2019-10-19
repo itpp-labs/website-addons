2d4751275f562bcd3bac0842b0e2adcf50f0c716
Artur Nigmatullin <nigmatullin@it-projects.info>
2019-10-19 20:59:29 +0500

    :euro:

--- website_multi_company_crm/__manifest__.py
+++ website_multi_company_crm/__manifest__.py
@@ -13,7 +13,7 @@
     "support": "apps@it-projects.info",
     "website": "https://it-projects.info/team/yelizariev",
     "license": "LGPL-3",
-    "price": 19.00,
+    "price": 10.00,
     "currency": "EUR",
 
     "depends": [
