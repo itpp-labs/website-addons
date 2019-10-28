aad9cd1be00e85ff58178ce1c8a2e16b2a934b16
Ilmir <ilmir.karamov@gmail.com>
2019-10-28 15:04:07 +0500

    :book: website_sale_checkout_store: update for the version

--- website_sale_checkout_store/__manifest__.py
+++ website_sale_checkout_store/__manifest__.py
@@ -2,6 +2,7 @@
 # Copyright 2016 Ilyas <https://github.com/ilyasProgrammer>
 # Copyright 2016-2017 Ivan Yelizariev <https://it-projects.info/team/yelizariev>
 # Copyright 2017-2018 Kolushov Alexandr <https://it-projects.info/team/KolushovAlexandr>
+# Copyright 2019 Karamov Ilmir <https://it-projects.info/team/ilmir-k>
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 
 {
@@ -9,7 +10,7 @@
     'summary': """Simplify checkout process by excluding shipping and/or payment information""",
     'category': 'eCommerce',
     'images': ['images/1.png'],
-    'version': '10.0.1.0.4',
+    'version': '10.0.1.0.5',
 
     'author': 'IT-Projects LLC',
     "support": "apps@it-projects.info",
