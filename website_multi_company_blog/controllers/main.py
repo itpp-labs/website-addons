d352f8263d2327453ec41c1d023e6439979582ea
Eugene Molotov <molotov@it-projects.info>
2019-11-29 07:45:57 +0000

    :hammer_and_wrench: website_multi_company_blog: reimplement domain computing

--- website_multi_company_blog/controllers/main.py
+++ website_multi_company_blog/controllers/main.py
@@ -5,6 +5,14 @@ from odoo import SUPERUSER_ID
 from odoo import http
 from odoo.addons.website_blog.controllers.main import WebsiteBlog
 from odoo.http import request
+from odoo.tools.safe_eval import safe_eval
+
+
+# we use this hack
+# after domain field in ir.rule was removed
+# https://github.com/odoo/odoo/commit/172a767e5f7add9c5e806e00e0146dc5f09e806b#diff-e487de633a7ef116c20a824d203973a3
+def get_domain_from_rule(rule):
+    return safe_eval(rule.domain_force, rule._eval_context())
 
 
 class WebsiteBlogExtended(WebsiteBlog):
@@ -15,7 +23,7 @@ class WebsiteBlogExtended(WebsiteBlog):
 
         if request.env.context.get('uid', 0) == SUPERUSER_ID:
             blog_env = blog_super.qcontext['blogs']
-            updated_blogs = blog_env.search(request.env.ref('website_multi_company_blog.blog_rule_all').domain)
+            updated_blogs = blog_env.search(get_domain_from_rule(request.env.ref('website_multi_company_blog.blog_rule_all')))
             blog_super.qcontext.update({
                 'blogs': updated_blogs,
             })
@@ -27,7 +35,7 @@ class WebsiteBlogExtended(WebsiteBlog):
 
         if request.env.context.get('uid', 0) == SUPERUSER_ID:
             post_env = blog_super.qcontext['posts']
-            updated_posts = post_env.search(request.env.ref('website_multi_company_blog.post_rule_all').domain)
+            updated_posts = post_env.search(get_domain_from_rule(request.env.ref('website_multi_company_blog.post_rule_all')))
             blog_super.qcontext.update({
                 'posts': updated_posts,
             })
