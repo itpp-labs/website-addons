b82620f18439c860867dc8b09c30d8bc930acde9
Eugene Molotov <molotov@it-projects.info>
2020-01-20 06:01:16 +0000

    :ambulance: website_sale_cache: active category was not highlighted

--- website_sale_cache/models.py
+++ website_sale_cache/models.py
@@ -1,6 +1,7 @@
 # Copyright 2017 Artyom Losev
 # Copyright 2018 Kolushov Alexandr <https://it-projects.info/team/KolushovAlexandr>
 # License MIT (https://opensource.org/licenses/MIT).
+# Copyright 2020 Eugene Molotov <https://it-projects.info/team/em230418>
 
 import logging
 
@@ -13,22 +14,15 @@ class WebsiteCategoryCache(models.Model):
 
     _inherit = "website"
 
-    @tools.cache()
-    def category_cache(self, cats, slug, keep, parent_category_ids, collapsed=False):
+    @tools.cache('current_cat', 'collapsed')
+    def category_cache(self, current_cat, cats, slug, keep, parent_category_ids, collapsed=False):
         if collapsed:
             template = "website_sale_cache.categories_collapsed_cache_template"
         else:
-            template = "website_sale_cache.categories_cache_template"
-        _logger.info("Product public categories were cached.")
-        return self.env["ir.ui.view"].render_template(
-            template,
-            {
-                "categories": cats,
-                "slug": slug,
-                "keep": keep,
-                "parent_category_ids": parent_category_ids,
-            },
-        )
+            template = 'website_sale_cache.categories_cache_template'
+        _logger.info('Product public categories were cached.')
+        return self.env['ir.ui.view'].render_template(template, {'category': current_cat, 'categories': cats, 'slug': slug, 'keep': keep,
+                                                                 'parent_category_ids': parent_category_ids})
 
     def action_update_cache(self):
         _logger.info("Cache has been cleared.")
