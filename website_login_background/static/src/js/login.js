cc53c0481eac8ad7b07c789269e8130ae9d444b7
Ivan Yelizariev <yelizariev@it-projects.info>
2020-02-12 06:26:17 +0000

    :green_heart: website_login_background/

--- website_login_background/static/src/js/login.js
+++ website_login_background/static/src/js/login.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+    "use strict";
     if ($(".oe_website_login_container").length) {
         var headerHeight = $("header").innerHeight();
         window.scrollTo(0, headerHeight);
