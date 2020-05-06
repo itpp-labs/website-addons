249f65e68ab73f7965cbccf1430abc7b3b65e3b4
Ivan Yelizariev <yelizariev@itpp.dev>
2020-05-06 11:39:26 +0500

    :sparkling_heart: 13.0 version is ready :+1:

--- website_multi_company_sale/models/sale_order.py
+++
@@ -1,7 +0,0 @@
-from odoo import fields, models
-
-
-class SaleOrder(models.Model):
-    _inherit = "sale.order"
-
-    website_id = fields.Many2one("website", "Online Order Website", readonly=True)
