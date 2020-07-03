b6c0583391f52f1a7c48de83a55ab53ccfc9803b
Denis Mudarisov <denis.mudarisov@gmail.com>
2020-07-03 17:23:14 +0500

    :cherries: change support address

--- payment_pagadito/__manifest__.py
+++ payment_pagadito/__manifest__.py
@@ -9,7 +9,7 @@
     "version": "12.0.1.0.0",
     "application": False,
     "author": "IT-Projects LLC, Ivan Yelizariev",
-    "support": "apps@it-projects.info",
+    "support": "apps@itpp.dev",
     "website": "https://it-projects.info/team/yelizariev",
     "license": "Other OSI approved licence",  # MIT
     # "price": 9.00,
