d6b509be5bc8c80b28b7f5874a91666763fb4cb8
Denis Mudarisov <denis.mudarisov@gmail.com>
2020-07-03 17:15:34 +0500

    :cherries: change copyrights

--- payment_pagadito/__manifest__.py
+++ payment_pagadito/__manifest__.py
@@ -1,5 +1,5 @@
 # Copyright 2019 Ivan Yelizariev <https://it-projects.info/team/yelizariev>
-# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+# License MIT (https://opensource.org/licenses/MIT).
 {
     "name": """Pagadito Payment Acquirer""",
     "summary": """Integration with Pagadito.com payment service""",
@@ -11,7 +11,7 @@
     "author": "IT-Projects LLC, Ivan Yelizariev",
     "support": "apps@it-projects.info",
     "website": "https://it-projects.info/team/yelizariev",
-    "license": "LGPL-3",
+    "license": "Other OSI approved licence",  # MIT
     # "price": 9.00,
     # "currency": "EUR",
     "depends": ["payment", "website_sale", "sale"],
