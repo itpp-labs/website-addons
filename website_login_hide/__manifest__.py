ebe3842eef0fbb2ff203c44c5eed367d8aa5a600
Mitchell Admin <itpp-bot@users.noreply.github.com>
2021-01-15 13:42:28 +0000

    :tada::one::three: website_login_hide previous commits history: https://github.com/itpp-labs/website-addons/commits/13.0/ir_rule_protected
    > Made via .github/workflows/DINAR-PORT.yml

---
+++ website_login_hide/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    "name": """No “Sign in” in Website""",
+    "summary": """""",
+    "category": "Website",
+    "images": [],
+    "version": "13.0.1.0.0",
+    "author": "IT-Projects LLC",
+    "support": "apps@itpp.dev",
+    "website": "https://it-projects.info",
+    "license": "Other OSI approved licence",  # MIT
+    # "price": 20.00,
+    # "currency": "EUR",
+    "depends": ["website"],
+    "external_dependencies": {"python": [], "bin": []},
+    "data": ["views.xml"],
+    "demo": [],
+    "installable": True,
+    "auto_install": False,
+}
