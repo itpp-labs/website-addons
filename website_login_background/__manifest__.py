cae2349fd083dd3a36ef7ca4ffebff062b4a298a
Mitchell Admin <itpp-bot@users.noreply.github.com>
2021-06-13 17:30:53 +0000

    :arrow_up::one::four: OCA/odoo-module-migrator
    close #621
    
    > Made via .github/workflows/DINAR-PORT.yml

--- website_login_background/__manifest__.py
+++ website_login_background/__manifest__.py
@@ -3,7 +3,7 @@
     "summary": """Random background image to your taste at the website login page""",
     "category": "Website",
     "images": ["images/5.png"],
-    "version": "13.0.1.0.3",
+    "version": "14.0.1.0.3",
     "author": "IT-Projects LLC",
     "support": "apps@itpp.dev",
     "website": "https://itpp.dev",
