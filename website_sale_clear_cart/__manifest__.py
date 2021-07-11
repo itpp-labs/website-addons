c5f0d24fc67ce783b4029712382e32a184d5cf15
Ivan Yelizariev <iel@odoo.com>
2021-07-11 17:15:34 +0200

    :book: fix typo in version tag

--- website_sale_clear_cart/__manifest__.py
+++ website_sale_clear_cart/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Clear cart button",
-    "vesion": "12.0.1.0.0",
+    "version": "12.0.1.0.0",
     "author": "IT-Projects LLC, Ivan Yelizariev",
     "license": "Other OSI approved licence",  # MIT
     "category": "eCommerce",
