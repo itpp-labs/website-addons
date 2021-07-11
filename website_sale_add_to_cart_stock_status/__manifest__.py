f97a1076f567bf577259871fd8241d93eeef537e
Ivan Yelizariev <iel@odoo.com>
2021-07-11 17:15:28 +0200

    :book: fix typo in version tag

--- website_sale_add_to_cart_stock_status/__manifest__.py
+++ website_sale_add_to_cart_stock_status/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Product status at website shop + Quick add items to shopping cart",
-    "vesion": "11.0.1.0.0",
+    "version": "11.0.1.0.0",
     "author": "IT-Projects LLC, Ivan Yelizariev",
     "license": "Other OSI approved licence",  # MIT
     "category": "eCommerce",
